f908a7f55c21accbffdd8f550908c32aceceaeed
Temeraire452 <frida963963@gmail.com>
2024-01-07 14:47:44 +0100

    Add project files.

---
+++ Models/Kur.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SkolaProjekt.Models;
+
+public partial class Kur
+{
+    public int KursId { get; set; }
+
+    public string? Kursnamn { get; set; }
+
+    public bool? Aktiv { get; set; }
+
+    public virtual ICollection<Betyg> Betygs { get; set; } = new List<Betyg>();
+}
---
+++ Models/Personal.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SkolaProjekt.Models;
+
+public partial class Personal
+{
+    public int PersonalId { get; set; }
+
+    public string? Namn { get; set; }
+
+    public string? Personnummer { get; set; }
+
+    public string? Befattning { get; set; }
+
+    public DateTime? Anställningsdatum { get; set; }
+
+    public string? Avdelning { get; set; }
+
+    public decimal? Lön { get; set; }
+
+    public virtual ICollection<Betyg> Betygs { get; set; } = new List<Betyg>();
+}
